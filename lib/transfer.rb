703566708535a17b9b223b97b24de75697a3d3c5
Kamille Longalong <k.pagibigan@gmail.com>
2020-06-04 06:49:14 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -20,8 +20,7 @@ class Transfer
       receiver.balance += amount
       @status = "complete"
     else
-     sender.balance < amount # || @status == "closed"
-     @status = "rejected"
+      @status = "rejected"
      return "Transaction rejected. Please check your account balance."
     end
   end
