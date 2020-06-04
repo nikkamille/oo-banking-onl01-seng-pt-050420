cc43c1c4a43122f49167844a7fbcc8cfa1e2c24b
Kamille Longalong <k.pagibigan@gmail.com>
2020-06-04 06:24:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,9 +15,13 @@ class Transfer
   end
   
   def execute_transaction
-    sender.balance -= amount
-    receiver.balance += amount
-    @status = "complete"
+    if self.valid?
+      sender.balance -= amount
+      receiver.balance += amount
+      @status = "complete"
+    else
+      
+    end
   end
   
 end
