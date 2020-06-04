d6e05ed8204736d861a5fc075047156cbce36f17
Kamille Longalong <k.pagibigan@gmail.com>
2020-06-04 06:16:05 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,17 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :amount, :status
+  attr_reader :sender, :receiver
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending" 
+  end
+  
+  def valid?
+    BankAccount.valid? 
+  end
+  
 end
